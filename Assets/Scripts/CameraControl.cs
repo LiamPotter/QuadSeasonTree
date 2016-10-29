11eed5cd9899255f75c1b88e3d50f794b967be17
Liam Potter <liamp62@gmail.com>
2016-10-29 16:02:34 +1000

    Added EVERYTHING
    -ITS ALL THERE

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using TriTools;
+public class CameraControl : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    public float damping;
+
+    private float horizontalInput;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        TriToolHub.Rotate(gameObject, TriToolHub.XYZ.Y, horizontalInput * Time.deltaTime*rotationSpeed, true, Space.World);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private GameObject tree;
+    public enum Seasons
+    {
+        Summer,
+        Autumn,
+        Winter,
+        Spring
+    }
+
+    public Seasons currentSeason;
+    public GameObject summerCritter, autumnCritter, winterCritter, springCritter;
+    // Use this for initialization
+    void Start ()
+    {
+        tree = GameObject.Find("Tree");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
